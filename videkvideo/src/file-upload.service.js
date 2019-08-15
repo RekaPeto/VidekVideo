aab66cb4be6ab90af8bb9233cafec5c4d2968963
Gergő Ádám <rapture047@gmail.com>
2019-08-15 17:25:07 +0200

    upload works

--- videkvideo/src/file-upload.service.js
+++ videkvideo/src/file-upload.service.js
@@ -5,10 +5,7 @@ const BASE_URL = 'https://localhost:44301'
 function upload(formData) {
     const url = `${BASE_URL}/api/video/`;
     return axios.post(url, formData)
-        .then(x => x.data)
         // add url field
-        .then(x => x.map(img => Object.assign({},
-            img, { url: `${BASE_URL}/images/${img.id}` })));
 }
 function wait(ms) {
     return (x) => {
